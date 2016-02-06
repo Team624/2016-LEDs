3756da31e91fba10485f88ec5beac73fdfc64a61
w0503980 <maddisonwillmott@gmail.com>
2016-02-06 11:26:45 -0600

    fixed and finished base code before animations

--- even_more_fancy_lights/even_more_fancy_lights.ino
+++ even_more_fancy_lights/even_more_fancy_lights.ino
@@ -4,14 +4,18 @@
 #define PIN 6
 #define PINZ 2
 
+#define MULTIPLEX_PIN A1
 
+#define ROBOT_STATE_PIN 12
+#define ROBOT_STATE_PIN_TWO 22
+#define ROBOT_STATE_PIN_THREE 28 /*???????????*/
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(120, PIN, NEO_BRG + NEO_KHZ800);
 Adafruit_NeoPixel stripz = Adafruit_NeoPixel(120, PINZ, NEO_GRB + NEO_KHZ800);
 bool on = true;
 #include "utilities.h"
 
-bool stay_white == false;
+bool stay_white = false;
 
 void setup() {
   pinMode(PIN, OUTPUT);
@@ -41,7 +45,7 @@ void loop() {
   uint8_t multi_read = analogRead(MULTIPLEX_PIN) >> 7; //grab 3 most signifigant bits?
   bool state_pin1 = digitalRead(ROBOT_STATE_PIN);
   bool state_pin2 = digitalRead(ROBOT_STATE_PIN_TWO);
-  bool state_pin3 = digitalRead(Robot_STATE_PIN_THREE);
+  bool state_pin3 = digitalRead(ROBOT_STATE_PIN_THREE);
   Serial.println();
   Serial.print(state_pin3);
   Serial.print(state_pin2);
@@ -71,7 +75,7 @@ void loop() {
     fillStripZ(stripz.Color(255, 0, 0), 255);
   }else if (robot_state == STATE_BROWNOUT) {
     fillStrip(strip.Color(70, 25, 0), 255);
-    fillStripz(stripz.Color(70, 25, 0), 255); //brown
+    fillStripZ(stripz.Color(70, 25, 0), 255); //brown
   }else if (robot_state == STATE_COOP) {
     coop_rainbowlaser();
   }else {
@@ -85,15 +89,15 @@ void loop() {
  stripz.show();
 }
 
-void raibow(uint8_t wait) {
+void rainbow(uint8_t wait) {
   uint16_t i, j;
   
    for (j = 0; j < 256; j++) {
      for (i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, WHeel((i + j) & 255));
+        strip.setPixelColor(i, Wheel((i + j) & 255));
    }
    for (i = 0; i < strip.numPixels(); i++) {
-     strip.setPixelColor(i, WHeel((i + j) & 255));
+     strip.setPixelColor(i, Wheel((i + j) & 255));
    }
 
   strip.show();
@@ -125,7 +129,7 @@ void laser()
 
 void rainbowlaser()
 {
-  uint32_t ii, c w,r ,o,y ,g ,b ,i ,v;
+  uint32_t ii, c, w, r, o, y, g ,b ,i ,v;
   r = strip.Color(255, 0, 0); //red
   o = strip.Color(225,128,0); //orange
   y = strip.Color(255,255,0); //yellow
@@ -177,8 +181,8 @@ void rainbowlaser()
   }
   else
   {
-    fillStrip(strip.Color(120, 120, 120), 255);
-    fillStripz(stripz.Color(120, 120, 120), 255);
+    fillStrip(strip.Color(120, 120, 120), 255); //grey
+    fillStripZ(stripz.Color(120, 120, 120), 255);
     strip.show();
     stripz.show();
   }
@@ -228,7 +232,7 @@ void coop_rainbowlaser()
       stripz.setPixelColor(i - 20, yz);
       stripz.setPixelColor(i - 24, oz);
       stripz.setPixelColor(i - 28, rz);
-      //stripz.setPixelColor(i-w,strip.Color(120,120,120));
+      //stripz.setPixelColor(i-w,strip.Color(120,120,120)); //grey
 
       strip.show();
       stripz.show();
@@ -240,7 +244,7 @@ void coop_rainbowlaser()
   {
     fillStrip(y, 255);
     fillStripZ(y, 255);
-    strip.show()
+    strip.show();
     stripz.show();
   }
 }
@@ -248,9 +252,9 @@ void coop_rainbowlaser()
 void bouncelaser()
 {
   uint16_t i, c, w;
-  c = strip.Color(0, 255, 0);
+  c = strip.Color(0, 255, 0); //green
 
-  w = 12
+  w = 12;
 
   for (i = 0; i < (strip.numPixels() + (w - 6)); i++)
   {
@@ -271,9 +275,118 @@ void bouncelaser()
   }
 }
 
+void pixelate()
+{
+  uint8_t i, px, pxs, pxss, pxz, pxsz, pxssz, color;
+  uint32_t g, w, b, c;
+  g = strip.Color(255, 255, 49); //greenish-yellow
+  b = strip.Color(255, 255, 0); //yellow
+  w = strip.Color(255, 255, 255); //white
+  c = strip.Color(0, 0, 0); //black
+
+  uint32_t gz, wz, bz, cz;
+  gz = stripz.Color(255, 255, 49);
+  bz = stripz.Color(255, 255, 0);
+  wz = stripz.Color(255, 255,255);
+  cz = stripz.Color(0, 0, 0);
+
+  px = random(0, strip.numPixels());
+  pxs = random(0, strip.numPixels());
+  pxss = random(0, strip.numPixels());
+  color = random(0,4) ;
+  if (color == 0)
+  {
+    strip.setPixelColor(px, g);
+    strip.setPixelColor(pxs, g);
+    strip.setPixelColor(pxss, g);
+
+    stripz.setPixelColor(px, gz);
+    stripz.setPixelColor(pxs, gz);
+    stripz.setPixelColor(pxss, gz);
+  }
+  else if (color ==1)
+  {
+    strip.setPixelColor(px, b);
+    strip.setPixelColor(pxs,b);
+    strip.setPixelColor(pxss, b);
+
+    stripz.setPixelColor(px, bz);
+    stripz.setPixelColor(pxs, bz);
+    stripz.setPixelColor(pxss, bz);
+  }
+  else if (color == 2)
+  {
+    strip.setPixelColor(px, c);
+    strip.setPixelColor(pxs, c);
+    strip.setPixelColor(pxss, c);
+
+    stripz.setPixelColor(px, cz);
+    stripz.setPixelColor(pxs, cz);
+    stripz.setPixelColor(pxss, cz);
+  }
+  else
+  {
+    strip.setPixelColor(px, w);
+    strip.setPixelColor(pxs, w);
+    strip.setPixelColor(pxss, w);
+
+    stripz.setPixelColor(px, wz);
+    stripz.setPixelColor(pxs, wz);
+    stripz.setPixelColor(pxss, wz);
+  }
+strip.show();
+stripz.show();
+}
+
+void lavalamp()
+{
+  uint16_t t = millis() / 40;
+  for (uint8_t i = 0; i < 122; i++) {
+    uint8_t brightness = sin((t + i) / 4.0) * 255;
+    uint8_t other = cos((t * 1.1 +i) / 3.5) *255;
+    brightness = brightness - other;
+    uint8_t rrr = cos((t / 1 + i /2) << 8) * 255;
+    strip.setPixelColor(i, strip.Color(0, brightness, 0));
+    stripz.setPixelColor(i, strip.Color(0, brightness, 0));
+  }
+  uint32_t g;
+  strip.show();
+  stripz.show();
+}
 
 
 
+
+
+
+
+
+
+
+
+
+void auton()
+{
+  fillStrip(strip.Color(0, 255, 0), 255);
+  fillStripZ(stripz.Color(0, 255, 0), 255);
+}
+
+/*void score()
+{
+  fillStrip(strip.Color(255,80,800, 255); //pink
+  fillStripZ(stripz.Color(255,80,80), 255);
+ }*/
+
+void score()
+{
+  for (byte i = 0; i < 64; i++) {
+    byte whiteness = i * 4;
+    fillStrip(strip.Color(whiteness, 255, whiteness), 255);
+    fillStripZ(stripz.Color(whiteness, 255, whiteness), 255);
+    strip.show();
+    stripz.show();
+  }
+}
 
 
 
