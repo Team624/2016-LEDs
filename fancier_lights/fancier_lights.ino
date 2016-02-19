3304fefe3c6541cfd56c586c8d632fef31ca4fda
j0$e9# n0rm@n <n0807701j2d2@gmail.com>
2016-02-18 19:57:49 -0600

    works on statusLights

--- fancier_lights/fancier_lights.ino
+++ fancier_lights/fancier_lights.ino
@@ -47,9 +47,9 @@ bool ballLoaded;
 bool alliance;
 
 void loop()
-{  
+{
   uint8_t multi_read = analogRead(MULTIPLEX_PIN) >> 31;
-  
+
   bool state_pin1 = digitalRead(ROBOT_STATE_PIN);
   bool state_pin2 = digitalRead(ROBOT_STATE_PIN_TWO);
   bool state_pin3 = digitalRead(ROBOT_STATE_PIN_THREE);
@@ -59,7 +59,7 @@ void loop()
   intakeActive = (multi_read >> 2) & 1;
   ballLoaded = (multi_read >> 3) & 1;
   alliance = (multi_read >> 4) & 1;
-  
+
   Serial.println();
   Serial.print(state_pin3);
   Serial.print(state_pin2);
@@ -94,59 +94,105 @@ void loop()
     fillStrip(strip.Color(255, 0, 0), 255);
     fillStripZ(stripz.Color(255, 0, 0), 255);
   }
-  else if(robot_state == STATE_CLIMB)
+  else if (robot_state == STATE_CLIMB)
   {
-    climbCRAZE(); 
+    // climbCRAZE();
   }
   else
   {
-    rainbow(0);  
+    rainbow(0);
   }
-  
+
   strip.show();
   stripz.show();
 }
 
-int sweg = 0;
-void climbCRAZE()
-{
-  clearStrip();
-  if(sweg == 1)
-    sweg = 0;
-  else
-    sweg = 1;
-    
-  int green = strip.Color(0,255,0);
-  int white = strip.Color(255,255,255);
-  for(int i = 0; i < strip.numPixels(); i ++)
-  {
-    strip.setPixelColor(i, white);
-    stripz.setPixelColor(i, white);
-  }
-  
-  for(int i = sweg; i < strip.numPixels(); i +=2)
-  {
-    strip.setPixelColor(i, green); 
-    stripz.setPixelColor(i, green);
-    delay(.5);
-  }
-}
-
-/*
+uint8_t sweg = 1;
 void statusLight()
 {
-  uint8_t blue = strip.Color(0, 0, 255);
-  uint8_t green = strip.Color(#06F000);
-  uint8_t red = strip.Color(255, 0, 0);
-  uint8_t yellow = strip.Color(255, 255, 0);
-
-  //intake indicators
-  for (uint8_t i = 0; i < strip.numPixels() / 6; i ++)
+  int blue = strip.Color(0, 0, 255);
+  int green = strip.Color(0, 255, 00);
+  int red = strip.Color(255, 0, 0);
+  int yellow = strip.Color(255, 255, 0);
+  int white = strip.Color(255,255,255);
+  
+  if(first_loop == true)
   {
-    if (ballLoaded == true)
+    if(!alliance)
+    {
+      fillStrip(255,red);
+      fillStripZ(255,red);
+      delay(50);
+    }
+    else
+    {
+      fillStrip(255,blue);
+      fillStrip(255,blue);
+      delay(50);
+    }
+    first_loop = false;
+  }
+  if(sweg == 0)
+    sweg = 1;
+  else
+    sweg = 0;
+  
+  //ball loaded
+  if(ballLoaded)
+  {
+    for (uint8_t i = 0; i < strip.numPixels() / 3; i ++)
+    {
+      if((i + sweg) % 2 == 0)
+      {
+        strip.setPixelColor(i,green);
+        stripz.setPixelColor(i,green);
+      }
+      else
+        strip.setPixelColor(i,white);
+        stripz.setPixelColor(i,white);
+    }
+  }
+  else
+  {
+    //intake indicator 1 Roller Active
+    for (uint8_t i = 0; i < strip.numPixels() / 6; i ++)
+    {
+      
+    }
+    //intake indicator 2 Roller Mode
+    for (uint8_t i = strip.numPixels() / 6; i < strip.numPixels() / 3; i ++)
+    {
+
+    }
+  }
+  
+  //drive indicator gear
+  for (uint8_t i = strip.numPixels() / 3; i < 2 * (strip.numPixels() / 3); i ++)
+  {
+    if (gear)
     {
       strip.setPixelColor(i, green);
       stripz.setPixelColor(i, green);
+    }
+    else
+    {
+      strip.setPixelColor(i, blue);
+      stripz.setPixelColor(i, blue);
+    }
+  }
+
+  //shooter indicator 1 shooter mode
+  for (uint8_t i = strip.numPixels() / 3; i < 5 * (strip.numPixels() / 6); i ++)
+  {
+    
+  }
+  //shooter indicator 2 shooter ready
+  for (uint8_t i = (5 * (strip.numPixels() / 6)); i < strip.numPixels(); i ++)
+  {
+    if(shooterReady)
+    {
+      strip.setPixelColor(i, blue);
+      stripz.setPixelColor(i, blue);
     }
     else
     {
@@ -154,36 +200,12 @@ void statusLight()
       stripz.setPixelColor(i, red);
     }
   }
-  for (uint8_t i = 0; i < strip.numPixels() / 3; i ++)
-  {
-
-  }
-  //drivers indicators
-  for (uint8_t = (strip.numPixels() / 3) * 2; i < (strip.numPixels() / 3) * 2; i ++)
-  {
-    if (gear == false)
-    {
-      strip.setPixelColor(i, blue);
-      stripz.setPixelColor(i, blue);
-    }
-    else
-    {
-      strip.setPixelColor(i, green);
-      stripz.setPixelColor(i, green);
-    }
-  }
-
-  //shooter indicators
-  for (uint8_t i = (strip.numPixels() / 3) * 2; i < (strip.numPixels() / 3) * 2; i ++)
-  {
-
-  }
 }
-*/
+
 void lavalamp()
 {
   uint16_t t = millis() / 40;
-  for (uint8_t i = 0; i < 33; i++) 
+  for (uint8_t i = 0; i < 33; i++)
   {
     uint8_t brightness = sin((t + i) / 4.0) * 255;
     uint8_t other = cos((t * 1.1 + i) / 3.5) * 255;
@@ -203,82 +225,82 @@ uint8_t pos2 = -1;
 
 void sweep()
 {
-  int green = strip.Color(0,255,0);
-  int blue = strip.Color(0,0,255);
-  int red = strip.Color(255,0,0);
-  int white = strip.Color(255,255,255);
-  
-  fillStripZ(0,0);
-  fillStrip(0,0);
-  
-  if(first_loop)
+  int green = strip.Color(0, 255, 0);
+  int blue = strip.Color(0, 0, 255);
+  int red = strip.Color(255, 0, 0);
+  int white = strip.Color(255, 255, 255);
+
+  fillStripZ(0, 0);
+  fillStrip(0, 0);
+
+  if (first_loop)
   {
     rate1 = 0;
     rate2 = 0;
     pos1 = 0;
     pos2 = strip.numPixels();
-    first_loop = false; 
+    first_loop = false;
   }
   else
   {
-    if(pos2 > strip.numPixels() / 2)
+    if (pos2 > strip.numPixels() / 2)
       rate2 --;
     else
       rate2 ++;
-    if(pos1 > strip.numPixels() / 2)  
+    if (pos1 > strip.numPixels() / 2)
       rate1 --;
     else
       rate1 ++;
-    
-    if(pos1 == 0)
+
+    if (pos1 == 0)
       delay(100);
-    
+
     pos1 += rate1;
     pos2 += rate2;
     delay(50);
-    
-    for(uint8_t i = pos1 - 8; i < pos1 + 8; i ++)
+
+    for (uint8_t i = pos1 - 8; i < pos1 + 8; i ++)
     {
-      strip.setPixelColor(i, green); 
+      strip.setPixelColor(i, green);
       stripz.setPixelColor(i, green);
     }
-   
-    for(uint8_t i = pos2 - 8; i < pos2 + 8; i ++)
+
+    for (uint8_t i = pos2 - 8; i < pos2 + 8; i ++)
     {
-      if(!alliance)
+      if (!alliance)
       {
         strip.setPixelColor(i, red);
-        stripz.setPixelColor(i, red); 
+        stripz.setPixelColor(i, red);
       }
       else
       {
-        strip.setPixelColor(i, blue); 
+        strip.setPixelColor(i, blue);
         stripz.setPixelColor(i, blue);
       }
     }
-   
-    for(uint8_t i = pos2 + 8; i < pos1 - 8; i ++)
+
+    for (uint8_t i = pos2 + 8; i < pos1 - 8; i ++)
     {
       strip.setPixelColor(i, white);
       stripz.setPixelColor(i, white);
     }
-  }    
+  }
 }
 
 void rainbow(uint8_t wait) {
   uint16_t i, j;
-  
-   for (j = 0; j < 256; j++) {
-     for (i = 0; i < strip.numPixels(); i++) {
-        strip.setPixelColor(i, Wheel((i + j) & 255));
-   }
-   for (i = 0; i < strip.numPixels(); i++) {
-     strip.setPixelColor(i, Wheel((i + j) & 255));
-   }
 
-  strip.show();
-  stripz.show();
-  delay(wait);
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+
+    strip.show();
+    stripz.show();
+    delay(wait);
   }
 }
 
@@ -294,13 +316,13 @@ void pixelate()
   uint32_t gz, wz, bz, cz;
   gz = stripz.Color(255, 255, 49);
   bz = stripz.Color(255, 255, 0);
-  wz = stripz.Color(255, 255,255);
+  wz = stripz.Color(255, 255, 255);
   cz = stripz.Color(0, 0, 0);
 
   px = random(0, strip.numPixels());
   pxs = random(0, strip.numPixels());
   pxss = random(0, strip.numPixels());
-  color = random(0,4) ;
+  color = random(0, 4) ;
   if (color == 0)
   {
     strip.setPixelColor(px, g);
@@ -311,10 +333,10 @@ void pixelate()
     stripz.setPixelColor(pxs, gz);
     stripz.setPixelColor(pxss, gz);
   }
-  else if (color ==1)
+  else if (color == 1)
   {
     strip.setPixelColor(px, b);
-    strip.setPixelColor(pxs,b);
+    strip.setPixelColor(pxs, b);
     strip.setPixelColor(pxss, b);
 
     stripz.setPixelColor(px, bz);
@@ -341,8 +363,8 @@ void pixelate()
     stripz.setPixelColor(pxs, wz);
     stripz.setPixelColor(pxss, wz);
   }
-strip.show();
-stripz.show();
+  strip.show();
+  stripz.show();
 }
 
 void happylittlewave()
@@ -353,9 +375,9 @@ void happylittlewave()
   dg = strip.Color(0, 100, 0); //dark green
   g = strip.Color(0, 255, 0); //green
   wg = strip.Color(75, 255, 75); //light green
-  colour = random(0,4);
+  colour = random(0, 4);
 
-  
+
   stripz.show();
   strip.show();
   delay(15);
@@ -363,20 +385,20 @@ void happylittlewave()
 
 void clearStrip()
 {
-  for(int i = 0; i < strip.numPixels(); i ++)
+  for (int i = 0; i < strip.numPixels(); i ++)
   {
     strip.setPixelColor(i, 0);
     strip.show();
     delay(1);
   }
-  for(int i = 0; i < stripz.numPixels(); i ++)
+  for (int i = 0; i < stripz.numPixels(); i ++)
   {
     stripz.setPixelColor(i, 0);
     stripz.show();
     delay(1);
   }
 }
- 
+
 void score()
 {
   for (byte i = 0; i < 64; i++) {
